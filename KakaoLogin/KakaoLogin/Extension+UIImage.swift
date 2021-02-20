4ea14b9645e73f358bfef01ba56af8d1b4abcad9
jesterz91 <jesterz91@naver.com>
2021-02-20 22:42:27 +0900

    Initial commit

---
+++ KakaoLogin/KakaoLogin/Extension+UIImage.swift
@@ -0,0 +1,13 @@
+//
+//  Extension+UIImage.swift
+//  KakaoLogin
+//
+//  Created by lee on 2021/02/20.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static let kakao_login: UIImage = UIImage(named: "kakao_login_medium_wide")!
+}
